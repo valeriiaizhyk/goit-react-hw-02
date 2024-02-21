38bc10530871e41f9a79ba6c0e175ed18672b352
valeriiaizhyk <137833881+valeriiaizhyk@users.noreply.github.com>
2024-02-20 23:10:35 -0700

    edit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,53 @@
+import { useState, useEffect} from 'react';
+import Description from '../Description/Description';
+import Options from '../Options/Options';
+import Feedback from '../Feedback/Feedback';
+import Notification from '../Notification/Notification';
+
+
+const App = () => {
+const [feedback, setFeeback] = useState(
+  () => JSON.parse(localStorage.getItem('saved-feedback')) ?? { good: 0, neutral: 0, bad: 0 }
+);
+
+useEffect(() =>{
+  window.localStorage.setItem('saved-feedback', JSON.stringify(feedback));
+},[feedback]);
+
+const updateFeedback = feedbackType =>{
+setFeeback({
+  ...feedback,
+  [feedbackType]: feedback[feedbackType] + 1,
+});
+};
+
+const resetFeedback = () => {
+  setFeeback({good:0, neutral:0, bad:0});
+};
+
+const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+
+const positiveReviews = Math.round(((feedback.good+ feedback.neutral) / totalFeedback) * 100)
+
+
+  return (
+    <>
+      <Description/>
+      <Options 
+      onUpdate={updateFeedback}
+      onReset={resetFeedback}
+      total={totalFeedback}
+      />
+      <Feedback
+      good={feedback.good}
+      neutral={feedback.neutral}
+      bad={feedback.bad}
+      total={totalFeedback}
+      positive={positiveReviews}
+      />
+      <Notification total={totalFeedback}/>
+    </>
+  )
+}
+
+export default App
